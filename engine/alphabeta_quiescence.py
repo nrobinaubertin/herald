3e7bb8c96e0603e6da8462ee3f26920c867f3d9c
Niels Robin Aubertin <niels.robin-aubertin@stilla.fr>
2022-06-09 12:38:29 +0200

    Switch to negaC with aspiration window

--- engine/alphabeta_quiescence.py
+++ engine/alphabeta_quiescence.py
@@ -7,35 +7,117 @@ from board import toUCI
 
 QUIESCENT_NODES = 0
 ALPHA_NODES = 0
+LEAF_NODES = 0
 
 def search_best_move(board, depth: int, eval_fn):
 
-    #search_hash = ""
-    #start_time = time.process_time()
+    search_hash = ""
+    start_time = time.process_time()
 
     global QUIESCENT_NODES
     global ALPHA_NODES
+    global LEAF_NODES
     best_move = None
     best_value = (VALUE_MAX * -1) - 1
     QUIESCENT_NODES = 0
     ALPHA_NODES = 0
+    LEAF_NODES = 0
     for move in board.moves():
         curr_board = board.copy()
         curr_board.push(move)
-        value = -alphaBeta(curr_board, VALUE_MAX * -1, VALUE_MAX, depth, eval_fn)
-        #print("SEARCH {}: {}\n------".format(toUCI(move), value))
-        #search_hash += f",{toUCI(move)}:{value}"
+        value = aspirationWindow(curr_board, board.eval, depth)
+        print(f"SEARCH {toUCI(move)}: {value}")
+        search_hash += f",{toUCI(move)}:{value}"
         if value > best_value:
             best_move = move
             best_value = value
 
-    #print(f"Alpha nodes: {ALPHA_NODES}")
-    #print(f"Qiescent nodes: {QUIESCENT_NODES}")
-    #print(f"search time: {time.process_time() - start_time}")
-    #print("hashtable size:{},req:{},hits:{}".format(len(hashtable.HASH_TABLE), hashtable.REQ, hashtable.HITS))
-    #print(f"search hash: {zlib.adler32(search_hash.encode())}")
+    print(f"Depth: {depth}")
+    print(f"Alpha nodes: {ALPHA_NODES}")
+    print(f"Qiescent nodes: {QUIESCENT_NODES}")
+    print(f"Leaf nodes: {LEAF_NODES}")
+    print(f"Search time: {time.process_time() - start_time}")
+    print(f"Hashtable size:{len(hashtable.HASH_TABLE)},req:{hashtable.REQ},hits:{hashtable.HITS}")
+    print(f"Search hash: {zlib.adler32(search_hash.encode())}")
+    print(f"Best move: {toUCI(best_move)} {best_value}")
     return best_move
 
+
+def aspirationWindow(board, guess: int, depth: int) -> int:
+    lower = guess - 50
+    upper = guess + 50
+    iteration = 0
+    while True:
+        iteration += 1
+        score = negaC(board, lower, upper, depth)
+        print(lower, upper, score)
+        if score > upper:
+            upper += 100 * (iteration ** 2)
+            continue
+        if score < lower:
+            lower -= 100 * (iteration ** 2)
+            continue
+        break
+    return score
+
+def negaC(board, min: int, max: int, depth: int) -> int:
+    score = min
+    while min < max:
+        alpha = (min + max + 1) // 2
+        score = alphaBetaFailSoft(board, alpha, alpha + 100, depth)
+        #print(min, max, score)
+        if score > alpha:
+            min = score
+        else:
+            max = score
+    return score
+
+#def MTDF(board, f: int, depth: int, eval_fn) -> int:
+#    gamma = f
+#    upper = f + 10000
+#    lower = f - 10000
+#    while lower < upper:
+#        gamma = (lower+upper+1)//2
+#        score = alphaBeta(board, lower, upper, depth, eval_fn)
+#        if score >= gamma:
+#            lower = score
+#        if score < gamma:
+#            upper = score
+#        print(lower, upper, gamma)
+#    return gamma
+
+def alphaBetaFailSoft(board, alpha: int, beta: int, depth: int) -> int:
+    if not len(board.pieces[PIECE.KING * board.turn]):
+        return VALUE_MAX * -1
+
+    if depth > 0:
+        mem = hashtable.get_value(board)
+        if mem is not None and mem.depth >= depth:
+            return mem.value
+
+    if depth == 0:
+        global LEAF_NODES
+        LEAF_NODES += 1
+        return board.eval * abs(board.turn) // board.turn
+
+    global ALPHA_NODES
+    ALPHA_NODES += 1
+
+    best_score = (VALUE_MAX * -1) - 1
+    for move in board.pseudo_legal_moves():
+        curr_board = board.copy()
+        curr_board.push(move)
+        score = -alphaBetaFailSoft(curr_board, -beta, -alpha, depth - 1)
+        if score >= beta:
+            return score
+        if score > best_score:
+            best_score = score
+            if score > alpha:
+                alpha = score
+
+    if depth > 0:
+        hashtable.set_value(board, hashtable.Mem(depth, best_score))
+    return best_score
 
 def alphaBeta(board, alpha: int, beta: int, depthleft: int, eval_fn) -> int:
     if not len(board.pieces[PIECE.KING * board.turn]):
@@ -48,7 +130,10 @@ def alphaBeta(board, alpha: int, beta: int, depthleft: int, eval_fn) -> int:
             return mem.value
 
     if depthleft == 0:
-        eval1 = eval_fn(board)
+        global LEAF_NODES
+        LEAF_NODES += 1
+        return board.eval * abs(board.turn) // board.turn
+        #eval1 = eval_fn(board)
         #eval2 = quiescent_alphaBeta(board, VALUE_MAX * -1, VALUE_MAX, eval_fn, 0)
         return eval1
         #return (eval1 + eval2) // 2
--- engine/bestmove.py
+++ engine/bestmove.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import board as b
 import evaluation
 import alphabeta_quiescence
@@ -6,9 +7,16 @@ import alphabeta_quiescence
 def bestMove(fen, depth):
     board = b.Board(fen)
     best_move = alphabeta_quiescence.search_best_move(board, depth, evaluation.eval)
-    #print(best_move)
     return b.toUCI(best_move)
 
 if __name__ == "__main__":
-    board = b.Board("r1bqkb1r/pppppppp/5n2/8/1n1PP3/P1N5/1PP2PPP/R1BQKBNR b KQkq - 0 4")
-    print(alphabeta_quiescence.search_best_move(board, 3, evaluation.eval))
+    board = b.Board("6kr/p1nprppp/3p4/1p2p3/4P1qP/1P1B4/PBPP1PPR/RQ4K1 b - - 9 34")
+    #board = b.Board("startpos")
+    start_time = time.process_time()
+    depth = 4
+    best_move = alphabeta_quiescence.search_best_move(board, depth, evaluation.eval)
+    #for i in range(depth + 1):
+    #    best_move = alphabeta_quiescence.search_best_move(board, i, evaluation.eval)
+    print()
+    print(f"Total Search time: {time.process_time() - start_time}")
+    print(f"Initial board eval: {board.eval}")
