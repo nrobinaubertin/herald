288e0fc808810b514dea146525ae8367767be1c8
Niels Robin-Aubertin <git@niels.fr>
2022-07-10 16:21:09 +0200

    Adjust time management a bit

--- m87.py
+++ m87.py
@@ -17,7 +17,7 @@ from engine.evaluation import eval_board
 from engine.data_structures import toUCI
 
 NAME = "M87"
-VERSION = "{} 0.9.3".format(NAME)
+VERSION = "{} 0.9.4".format(NAME)
 AUTHOR = "nrobinaubertin"
 CURRENT_BOARD = Board("startpos")
 CURRENT_PROCESS = None
@@ -38,14 +38,14 @@ def bestMove(board, max_time=0, max_depth=0, eval_guess=0, rand_count=1):
                 + f"nodes {best.nodes} "
                 + (
                     "nps "
-                    + str(int(best.nodes * 1e9 // max(0.0001, best.time)))
+                    + str(int(best.nodes * 1e9 // max(0.001, best.time)))
                     + " "
                     if best.time > 0
                     else ""
                 )
                 + f"pv {' '.join([toUCI(x) for x in best.pv])}"
             )
-            if used_time * 5 > (max_time - 1) // max(20, 80 - board.full_move) * 1000:
+            if used_time * 5 > (max_time - 1) // max(5, 40 - board.full_move) * 1000:
                 break
     else:
         for i in range(max_depth + 1):
