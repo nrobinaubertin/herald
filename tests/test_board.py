a29a3c4c7338350a2d85faec4b1bf946c74aa1e2
Niels Robin-Aubertin <git@niels.fr>
2022-08-26 17:18:13 +0200

    Fix test_board

--- tests/test_board.py
+++ tests/test_board.py
@@ -4,28 +4,28 @@ We test our move generation using the perft algorithm
 """
 
 import unittest
-from src.engine.board import Board
+import src.engine.board as board
+from src.engine.data_structures import Board
 
 
 class TestAlgorithms(unittest.TestCase):
-    def execute(self, board: Board, depth: int):
+    def execute(self, b: Board, depth: int):
         if depth == 1:
-            return len(list(board.pseudo_legal_moves()))
+            return len(list(board.pseudo_legal_moves(b)))
 
         nodes = 0
-        for move in board.moves():
-            curr_board = board.copy()
-            curr_board.push(move)
+        for move in board.moves(b):
+            curr_board = board.push(b, move)
             nodes += self.execute(curr_board, depth - 1)
 
         return nodes
 
     def test_perft(self):
-        nodes = self.execute(Board("startpos"), 4)
+        nodes = self.execute(board.from_fen("startpos"), 4)
         # returns 197742
         # which is the correct number + 469 checks - 8 mats
         self.assertEqual(nodes, 197281)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
