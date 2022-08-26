4a2a3256a1c1fdbd827ae643b5513f0b7e1d3408
Niels Robin-Aubertin <git@niels.fr>
2022-08-26 17:18:28 +0200

    Add alphabeta_mo_tt to tested algs

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -6,28 +6,43 @@ We test algorithms by comparing results to the minimax algorithm who's well know
 from collections import deque
 import unittest
 from .win_at_chess import win_at_chess
-from src.engine.board import Board
-from src.engine.algorithms import minimax, alphabeta, alphabeta_mo
-from src.engine.evaluation import VALUE_MAX
+import src.engine.board as board
+from src.engine.algorithms import minimax, alphabeta, alphabeta_mo, alphabeta_mo_tt
+from src.engine.constants import VALUE_MAX
+from src.engine.transposition_table import TranspositionTable
 
 
 class TestAlgorithms(unittest.TestCase):
     def test_alphabeta(self):
         depth = 2
         for fen in win_at_chess[:10]:
-            minimax_result = minimax(Board(fen), depth, deque())
-            alphabeta_result = alphabeta(Board(fen), -VALUE_MAX, VALUE_MAX, depth, deque())
+            minimax_result = minimax(board.from_fen(fen), depth, deque())
+            alphabeta_result = alphabeta(
+                board.from_fen(fen), -VALUE_MAX, VALUE_MAX, depth, deque()
+            )
             self.assertEqual(minimax_result.value, alphabeta_result.value)
             self.assertEqual(minimax_result.pv, alphabeta_result.pv)
 
     def test_alphabeta_mo(self):
         depth = 2
         for fen in win_at_chess[:10]:
-            minimax_result = minimax(Board(fen), depth, deque())
-            alphabeta_mo_result = alphabeta_mo(Board(fen), -VALUE_MAX, VALUE_MAX, depth, deque())
+            minimax_result = minimax(board.from_fen(fen), depth, deque())
+            alphabeta_mo_result = alphabeta_mo(
+                board.from_fen(fen), -VALUE_MAX, VALUE_MAX, depth, deque()
+            )
             self.assertEqual(minimax_result.value, alphabeta_mo_result.value)
             self.assertEqual(minimax_result.pv, alphabeta_mo_result.pv)
 
+    def test_alphabeta_mo_tt(self):
+        depth = 2
+        for fen in win_at_chess[:10]:
+            minimax_result = minimax(board.from_fen(fen), depth, deque())
+            alphabeta_mo_tt_result = alphabeta_mo_tt(
+                board.from_fen(fen), -VALUE_MAX, VALUE_MAX, depth, deque(), TranspositionTable({})
+            )
+            self.assertEqual(minimax_result.value, alphabeta_mo_tt_result.value)
+            self.assertEqual(minimax_result.pv, alphabeta_mo_tt_result.pv)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
