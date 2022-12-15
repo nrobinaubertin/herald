5f9ee53d3a12b457c636f49e6631e93f3897c802
Niels Robin-Aubertin <git@niels.fr>
2022-12-15 22:07:44 +0100

    Fix algorithms tests

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -7,54 +7,20 @@ from collections import deque
 import unittest
 from .win_at_chess import win_at_chess
 import src.engine.board as board
-from src.engine.algorithms import minimax, alphabeta, negac
+from src.engine.algorithms import minimax, alphabeta
 from src.engine.constants import VALUE_MAX
 from src.engine.evaluation import eval_simple
 from src.engine import move_ordering
-from src.engine.data_structures import to_uci
+from src.engine.data_structures import to_uci, MoveType
 
 
 class TestAlgorithms(unittest.TestCase):
-    fens = win_at_chess[:50]
-
-    # This test equivalence between negac and alphabeta w/ mvv-lva
-    def test_negac(self):
-        depth = 4
-        for fen in self.fens:
-            negac_result = negac(
-                board.from_fen(fen),
-                depth,
-                deque(),
-                eval_simple,
-                -VALUE_MAX,
-                VALUE_MAX,
-                None,
-                move_ordering.mvv_lva,
-            )
-            alphabeta_mvv_lva_result = alphabeta(
-                board.from_fen(fen),
-                depth,
-                deque(),
-                eval_simple,
-                -VALUE_MAX,
-                VALUE_MAX,
-                None,
-                move_ordering.mvv_lva,
-            )
-            self.assertEqual(
-                negac_result.value,
-                alphabeta_mvv_lva_result.value
-            )
-            self.assertEqual(
-                f"{fen}: {','.join([to_uci(x) for x in negac_result.pv])}",
-                f"{fen}: {','.join([to_uci(x) for x in alphabeta_mvv_lva_result.pv])}",
-            )
 
     # This test equivalence between raw alphabeta
     # and alphabeta with mvv_lva move ordering
     def test_mvv_lva(self):
-        depth = 4
-        for fen in self.fens:
+        depth = 3
+        for fen in win_at_chess[:50]:
             alphabeta_result = alphabeta(
                 board.from_fen(fen),
                 depth,
@@ -63,7 +29,9 @@ class TestAlgorithms(unittest.TestCase):
                 -VALUE_MAX,
                 VALUE_MAX,
                 None,
-                None,
+                move_ordering.no_ordering,
+                MoveType.LEGAL,
+                {},
             )
             alphabeta_mvv_lva_result = alphabeta(
                 board.from_fen(fen),
@@ -74,6 +42,8 @@ class TestAlgorithms(unittest.TestCase):
                 VALUE_MAX,
                 None,
                 move_ordering.mvv_lva,
+                MoveType.LEGAL,
+                {},
             )
             self.assertEqual(
                 alphabeta_mvv_lva_result.value,
@@ -87,12 +57,15 @@ class TestAlgorithms(unittest.TestCase):
     # This test equivalence between raw alphabeta and minimax
     def test_alphabeta(self):
         depth = 3
-        for fen in self.fens:
+        for fen in win_at_chess[:50]:
             minimax_result = minimax(
                 board.from_fen(fen),
                 depth,
                 deque(),
-                eval_simple
+                eval_simple,
+                0, 0, None,
+                move_ordering.no_ordering,
+                MoveType.LEGAL,
             )
             alphabeta_result = alphabeta(
                 board.from_fen(fen),
@@ -102,7 +75,9 @@ class TestAlgorithms(unittest.TestCase):
                 -VALUE_MAX,
                 VALUE_MAX,
                 None,
-                None,
+                move_ordering.no_ordering,
+                MoveType.LEGAL,
+                {},
             )
             self.assertEqual(minimax_result.value, alphabeta_result.value)
             self.assertEqual(
