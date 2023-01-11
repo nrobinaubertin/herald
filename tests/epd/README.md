11cee0e22f2aa09c2e26ef7acc8dcb0c2713efee
Niels Robin-Aubertin <git@niels.fr>
2023-01-12 00:10:37 +0100

    Fix unit tests

---
+++ tests/epd/README.md
@@ -0,0 +1,6 @@
+# Test positions
+
+links:
+- https://www.chessprogramming.org/Test-Positions
+- http://www.bergbomconsulting.se/chess/testsuites.html
+- https://www.arasanchess.org/testsuite.shtml
--- tests/unit/test_board.py
+++ tests/unit/test_board.py
@@ -1,8 +1,14 @@
+import os
 import pytest
 
 from herald import board
 
-from ..fen.win_at_chess import win_at_chess
+win_at_chess = ["rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 0"]
+
+with open("tests/epd/wac.epd", "r") as wacfile:
+    for line in wacfile:
+        epd = line.split()
+        win_at_chess.append(" ".join(epd[:4]) + " 0 0")
 
 
 @pytest.mark.parametrize("fen", win_at_chess)
